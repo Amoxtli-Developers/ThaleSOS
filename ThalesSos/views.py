dc2ee50ea096f63c8b19fa0b050920b4e0ef8609
salomon <mtzelisalomon@gmail.com>
2023-10-27 17:32:50 -0600

    google speech-to-text

--- ThalesSos/views.py
+++ ThalesSos/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .audio_transcription import transcribe_audio  # Importando la función de transcripción
+import os  # Import the os module
 
-# Create your views here.
+def transcribe_google(request):
+    if request.method == 'POST':
+        audio_file = request.FILES.get('audio_file')
+        if audio_file:
+            # Handle the uploaded audio file
+            os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/salomon/Desktop/imperial-data-403319-ab8beada07d0.json'
+            transcript = transcribe_audio(audio_file.read())
+            if transcript is not None:
+                return HttpResponse(f"Transcript: {transcript}")
+            else:
+                return HttpResponse("Transcription failed.")
+        else:
+            return HttpResponse("No audio file uploaded.")
+    return render(request, 'transcription_form.html')
