12e0e84f0b31d53cf56f87ba4db88780cb6bf1bc
Isaac Jacinto Ruiz <isaacjacintoruiz@gmail.com>
2023-10-27 17:19:24 -0600

    Template folder add

--- ThalesSos/views.py
+++ ThalesSos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def mi_vista(request):
+    contexto = {
+        'nombre_del_sitio': 'Mi Sitio Web',
+        'fecha_actual': '27 de octubre de 2023'
+    }
+    return render(request, 'mi_template.html', contexto)
