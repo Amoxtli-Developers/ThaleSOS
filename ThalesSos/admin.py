a75ad52a40bb91738dad443b74e8c36c2d387f41
Alpha0531 <A01659734@itesm.mx>
2023-10-27 16:25:11 -0600

    Model Warning & Categorie created

--- ThalesSos/admin.py
+++ ThalesSos/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Warning, Categorie
 
 # Register your models here.
+
+
+class WarningAdmin(admin.ModelAdmin):
+    list_display = ('id', 'keywords', 'category')  # Campos que se mostrarán en la lista
+    list_filter = ('category',)  # Campos por los cuales filtrar
+    search_fields = ('keywords',)  # Campos que se pueden buscar
+
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'message')
+    search_fields = ('name', 'message')
+
+
+admin.site.register(Warning, WarningAdmin)
+admin.site.register(Categorie, CategorieAdmin)
--- ThalesSos/models.py
+++ ThalesSos/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Adverts(models.Model):
+class Warning(models.Model):
     id = models.AutoField(primary_key=True)
-    words = models.CharField(max_length=100)
-    category = models.ForeignKey('Categories', on_delete=models.CASCADE)
+    keywords = models.CharField(max_length=100)
+    category = models.ForeignKey('Categorie', on_delete=models.CASCADE)
 
-class Categories(models.Model):
+class Categorie(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     message = models.CharField(max_length=100)
