8aa9e2a74e762745a8d1baf6f0758f17a2c93a41
Sofia Donlucas Bañuelos <71408638+Soff17@users.noreply.github.com>
2023-10-28 00:08:21 -0600

    admin page added

--- ThalesSos/urls.py
+++ ThalesSos/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     # Add more URL patterns as needed
     path('administrador', views.administrador, name='administrador'),
     path('home', views.home, name='home'),
+    path('delete_warning/<int:id>/', views.delete_warning, name='delete_warning'),
+    path('delete_categorie/<int:id>/', views.delete_categorie, name='delete_categorie'),
+    path('add_category/', views.add_category, name='add_category'),
+    path('add_warning/', views.add_warning, name='add_warning'),
+    path('categories/update/', views.update_category, name='update_category'),
+    path('update_warning/', views.update_warning, name='update_warning'),
 ]
--- ThalesSos/views.py
+++ ThalesSos/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .audio_transcription import transcribe_audio  # Importando la función de transcripción
 import os  # Import the os module
+from .models import Categorie, Warning
+from django.shortcuts import redirect, get_object_or_404
 
 def transcribe_google(request):
     if request.method == 'POST':
@@ -19,7 +21,88 @@ def transcribe_google(request):
     return render(request, 'transcription_form.html')
 
 def administrador(request):
-      return render(request, 'administrador.html')
+    categories = Categorie.objects.all()
+    total_categorias = categories.count()
+    warnings = Warning.objects.all()
+    total_warnings = warnings.count()
+    return render(request, 'administrador.html', {
+        'warnings': warnings,
+        'total_warnings': total_warnings,
+        'categories': categories,
+        'total_categorias': total_categorias
+    })
+
+def delete_warning(request, id):
+    warning = Warning.objects.get(pk=id)
+    warning.delete()
+    return redirect(administrador)
+
+def delete_categorie(request, id):
+    categorie = Categorie.objects.get(pk=id)
+    categorie.delete()
+    return redirect(administrador)
+
+def add_category(request):
+    print("Vista add_category llamada")  # Para saber que la vista fue llamada
+
+    if request.method == 'POST':
+        category_name = request.POST.get('categoryName')
+        category_message = request.POST.get('categoryMensaje')
+        
+        print(f"Nombre de categoría recibido: {category_name}")  # Verificar el dato recibido
+        print(f"Mensaje de categoría recibido: {category_message}")  # Verificar el dato recibido
+        
+        Categorie.objects.create(name=category_name, message=category_message)
+        
+        return redirect('administrador')
+
+
+
+def add_warning(request):
+    categories = Warning.objects.all()
+
+    if request.method == 'POST':
+        keywords = request.POST.get('keywords')
+        category_id = request.POST.get('category')
+        category = Categorie.objects.get(id=category_id)
+
+        Warning.objects.create(keywords=keywords, category=category)
+        return redirect('administrador')  # Ejemplo: al dashboard o al listado de warnings.
+
+    return render(request, 'administrador.html', {'categories': categories})
+
+def update_category(request):
+    if request.method == "POST":
+        cat_id = request.POST.get('categoryId')
+        cat_name = request.POST.get('categoryName-edited')
+        cat_message = request.POST.get('categoryMensaje-edited')
+
+        category = get_object_or_404(Categorie, id=cat_id)
+        category.name = cat_name
+        category.message = cat_message
+        category.save()
+
+        return redirect('administrador')
+
+def update_warning(request):
+    if request.method == "POST":
+        warning_id = request.POST.get('category_id')
+        keywords = request.POST.get('keywords-id')
+        category_id = request.POST.get('category-nueva')
+
+        # Obtener el objeto Warning basado en el ID
+        warning_instance = Warning.objects.get(id=warning_id)
+
+        # Actualizar los campos
+        warning_instance.keywords = keywords
+        warning_instance.category_id = category_id
+        warning_instance.save()
+
+        # Redireccionar (por ejemplo, a la página principal o donde prefieras)
+        return redirect('administrador')
+
+    # Si no es POST, mostrar el formulario o cualquier otra vista.
+    return render(request, 'administrador.html', {})
 
 def home(request):
       return render(request, 'home.html')
